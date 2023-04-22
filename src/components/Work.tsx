82a29c24260286871ec254a50e0af49329c97397
nncrvo <nunocrmartins@gmail.com>
2023-04-22 12:47:43 +0200

    work page almost done

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,8 @@
 import './Work.css';
 import { useNavigate } from 'react-router-dom';
 import todoAppImg from './assets/images/todoapp.png';
+import milkAppImg from './assets/images/milkapp.png';
+import flightsAppImg from './assets/images/flightsapp.png';
 
 const Work = () => {
     const navigate = useNavigate();
@@ -16,16 +18,21 @@ const Work = () => {
                 </div>
                 <section className='workCardsContainer'>
                     <div className='workCard'>
-                        <img src={todoAppImg} alt="todoapp" />
+                        <img className='todoAppImg' src={todoAppImg} alt="todoapp" />
+                        {/* <p className='imgLink'>https://deluxe-nasturtium-b5b5b8.netlify.app/</p> */}
+                        <p className='imgLink'>Link to website</p>
+                        <h2 className='workTitle'>ToDo App</h2>
+                        <p className='workDescription'>Simple todo app created with React with a retro inspired design and a more unusual layout.</p>
                     </div>
                     <div className='workCard'>
-
+                        <img src={milkAppImg} alt="milkapp" />
+                        <h2 className='workTitle'>E-Commerce App</h2>
+                        <p className='workDescription'>Fictional milk products store app with search/filter/cart management functionalities. Fullstack Typescript app with a simple NodeJs + Express server and a React frontend.</p>
                     </div>
                     <div className='workCard'>
-
-                    </div>
-                    <div className='workCard'>
-
+                        <img src={flightsAppImg} alt="flightsapp" />
+                        <h2 className='workTitle'>Flight Finder App</h2>
+                        <p className='workDescription'>Flight finder app with a focus on cleaner design. Fullstack app with a NodeJs + Express server and a React frontend.</p>
                     </div>
                 </section>
             </div>
