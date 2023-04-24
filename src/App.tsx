6fadb996d2b38cf2dc8d1da9db74c4a1353b8033
nncrvo <nunocrmartins@gmail.com>
2023-04-24 14:32:56 +0200

    image loading fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,15 @@ import Home from './components/Home';
 import About from './components/About';
 import Work from './components/Work';
 import Loader from './components/Loader';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import background from './components/assets/images/1-grain.jpg';
 
 function App() {
   const [isLoading, setIsLoading] = useState<boolean>(true);
+  const backgroundRef = useRef<HTMLImageElement>(null)
 
   const onBackgroundLoad = () => {
+    backgroundRef.current!.style.display = 'block';
     setTimeout(() => {
       setIsLoading(false);
     }, 500)
@@ -18,7 +20,7 @@ function App() {
   
   return (
     <div className="App">
-      <img className='background' src={background} alt="background" onLoad={onBackgroundLoad}/>
+      <img className='background' src={background} alt="background" onLoad={onBackgroundLoad} ref={backgroundRef}/>
       {isLoading ? <Loader /> : <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/about' element={<About/>} />
